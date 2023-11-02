d18026f2ac41170dc77f2a7deb789f7b713a60b1
MehGrrr <149424832+MehGrrr@users.noreply.github.com>
2023-11-03 01:41:44 +0500

    Update Byndyusoft_Telegraam link.cy.js

--- Byndyusoft_Telegraam link.cy.js
+++ Byndyusoft_Telegraam link.cy.js
@@ -10,35 +10,6 @@ describe('Check the Telegram link', () => {
     cy.get('section[class="knowMore"]')
     cy.contains('Заказать презентацию').click()
     cy.contains('в Телеграм').should('have.attr', 'href', 'http://t.me/alexanderbyndyu')
-  })
-  })  
-})
-
-//
-//it ('Byndyusoft', () => {
-  //  cy.visit('https://byndyusoft.com/')  
-  //  cy.scrollTo('bottom')
-  //  cy.contains('Заказать презентацию').click()
-  //  cy.contains('в Телеграм').should('have.attr', 'href', 'http://t.me/alexanderbyndyu') 
-  //});
-
-
-
-
-
-
-//.invoke('removeAttr', 'target')
-//it ('Contacts', () => {
-//    cy.visit('https://byndyusoft.com/')
-//    cy.scrollTo('bottom')
-//    cy.contains('Заказать презентацию').click()
-//    cy.contains('в Телеграм').click()
-//cy.get('textarea[type="search"]').click().type('Byndyusoft{enter}')
-//cy.contains('https://byndyusoft.com').invoke('removeAttr', 'target').click()
-
-
-//it ('Contacts', () => {
-  //   cy.visit('https://byndyusoft.com/')
-  //   cy.scrollTo('bottom')
-  //   cy.contains('Заказать презентацию').click()
-  //   cy.get('a [href= "https://t.me/alexanderbyndyu"]').click()
+    });
+  });  
+});
