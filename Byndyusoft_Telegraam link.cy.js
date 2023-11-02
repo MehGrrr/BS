18ccc88a7a4a891599231f047bc5116643e79524
MehGrrr <149424832+MehGrrr@users.noreply.github.com>
2023-11-03 01:56:53 +0500

    Update Byndyusoft_Telegraam-link.cy.js

--- Byndyusoft_Telegraam link.cy.js
+++ Byndyusoft_Telegraam link.cy.js
@@ -6,10 +6,9 @@ describe('Check the Telegram link', () => {
     cy.get('#search a').first().invoke('removeAttr', 'target').click()
     
     cy.origin('https://byndyusoft.com/', () => { 
-    cy.scrollTo('bottom')
-    cy.get('section[class="knowMore"]')
+    cy.get('section[class="knowMore"]').scrollIntoView()
     cy.contains('Заказать презентацию').click()
     cy.contains('в Телеграм').should('have.attr', 'href', 'http://t.me/alexanderbyndyu')
-    });
-  });  
-});
+    })
+  })  
+})
