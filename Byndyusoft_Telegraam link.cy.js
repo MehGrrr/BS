1e9d29559dba2014b89d07d98444efb4ce64de10
Anna Saratovtseva <149424832+MehGrrr@users.noreply.github.com>
2023-11-17 14:57:48 +0500

    Update Byndyusoft_Telegraam link.cy.js

--- Byndyusoft_Telegraam link.cy.js
+++ Byndyusoft_Telegraam link.cy.js
@@ -3,7 +3,7 @@ describe('Check the Telegram link', () => {
   it('Find-open-check link', () => {
     cy.visit('https://www.google.ru/')  
     cy.get('textarea[type="search"]').click().type('Byndyusoft{enter}')
-    cy.get('#search a').first().invoke('removeAttr', 'target').click().wait(500)
+    cy.get('#search a').first().invoke('removeAttr', 'target').click()
     //использую origin, т.к. в cypress не реализована функция для работы с redirect links и новыми вкладками, которые открываются при клике на ссылку в результатах запроса google
     cy.origin('https://byndyusoft.com/', () => { 
         cy.get('section[class="knowMore"]').scrollIntoView().find('span[class="btn btn--lg btn--info js-popup-callback-show"]').click()
