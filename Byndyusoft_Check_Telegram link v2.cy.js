27b9ab6b5747a12381b9c405db15730632063ade
Anna Saratovtseva <149424832+MehGrrr@users.noreply.github.com>
2024-06-18 08:39:34 +0500

    Create Byndyusoft_Check_Telegram link v2.cy.js

---
+++ Byndyusoft_Check_Telegram link v2.cy.js
@@ -0,0 +1,22 @@
+describe("Byndyusoft test task", () => {
+  it("have correct tg link", () => {
+    cy.visit("https://www.google.ru");
+
+    cy.get("#APjFqb").type("Byndyusoft");
+    cy.get(".aajZCb > .lJ9FBc > center > .gNO89b").click();
+    cy.get(
+      '.tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .notranslate > .q0vns > .CA5RN > .byrV5b > .tjvcx'
+    ).then(($el) => {
+      const url = $el.text();
+
+      cy.visit(url);
+
+      cy.origin(url, () => {
+        cy.get(".knowMore__container > .btn").click();
+        cy.get(".popup-callback__contacts-tg")
+          .should("have.attr", "href")
+          .and("eq", "https://t.me/alexanderbyndyu");
+      });
+    });
+  });
+});
